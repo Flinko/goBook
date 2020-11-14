61acd135953126fd66573ccd04375c36f2ce089d
Alina <alina@omc.co.il>
2020-11-14 14:45:51 +0200

    first exersices page 8

---
+++ chap1/ex1.1/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	var s string
+	for idx, arg := range os.Args {
+		s += strconv.Itoa(idx) + " " + arg + "\n"
+	}
+	fmt.Println(s)
+	start := time.Now()
+	ex13(os.Args)
+	end := time.Now().Sub(start)
+	fmt.Printf("regular took: %v\n", end.Nanoseconds())
+	start = time.Now()
+	ex13Join(os.Args)
+	end = time.Now().Sub(start)
+	fmt.Printf("with join took: %v\n", end.Nanoseconds())
+}
+
+func ex13(args []string) {
+	var s string
+	for idx, arg := range os.Args {
+		s += strconv.Itoa(idx) + " " + arg + "\n"
+	}
+	fmt.Println(s)
+}
+
+//if we a lot of values strings.Join more efficient them +=
+func ex13Join(args []string) {
+	var s string
+	for idx, arg := range os.Args {
+		s = strings.Join([]string{s, strconv.Itoa(idx), arg}, " ")
+	}
+	fmt.Println(s)
+}
